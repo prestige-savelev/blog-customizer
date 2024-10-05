02cd96d20e40adc103bd7bac3210e7a15f8dbae0
prestige-savelev <danilcaheleh0@gmail.com>
2024-10-05 22:12:04 +0300

    fix/startKit

---
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -0,0 +1,21 @@
+import { ArrowButton } from 'src/ui/arrow-button';
+import { Button } from 'src/ui/button';
+
+import clsx from 'clsx';
+import styles from './ArticleParamsForm.module.scss';
+
+export const ArticleParamsForm = () => {
+	return (
+		<>
+			<ArrowButton isOpen={true} onClick={() => {}} />
+			<aside className={clsx(styles.container, styles.container_open)}>
+				<form className={styles.form}>
+					<div className={styles.bottomContainer}>
+						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button title='Применить' htmlType='submit' type='apply' />
+					</div>
+				</form>
+			</aside>
+		</>
+	);
+};
