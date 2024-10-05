55f02add2c01ad356b9f205b77891a73f4eea182
prestige-savelev <danilcaheleh0@gmail.com>
2024-10-05 22:55:07 +0300

    add/openForm/OutsideClickClose

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,6 +3,8 @@ import { Button } from 'src/ui/button';
 
 import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { useRef } from 'react';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 
 type ArticleParamsFormType = {
 	isOpen: boolean;
@@ -13,10 +15,22 @@ export const ArticleParamsForm = (props: ArticleParamsFormType) => {
 	// Фунуция изменения состояния открытия формы
 	const toggleOpen = () => props.setOpen(!props.isOpen);
 
+	// Хук для хранения разметки aside формы
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	// Кастомный хук для закрытия при клике вне экрана
+	useOutsideClickClose({
+		isOpen: props.isOpen,
+		rootRef: ref,
+		onClose: () => toggleOpen,
+		onChange: props.setOpen,
+	});
+
 	return (
 		<>
 			<ArrowButton isOpen={props.isOpen} onClick={toggleOpen} />
 			<aside
+				ref={ref}
 				className={clsx(styles.container, {
 					[styles.container_open]: props.isOpen,
 				})}>
